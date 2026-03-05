f41dcfa6a6950645c0d3becf08539f8c4d05a7df
xuelinggong <18365553626@163.com>
2026-03-05 13:32:11 +0800

    Update gtedge.py

--- gtedge.py
+++ gtedge.py
@@ -157,8 +157,8 @@ def disp_to_edge(
 
 
 def process_sceneflow_disp(
-    root_disp="/home/qi.xiong/StereoMatching/IGEV-Improve/data/sceneflow/disparity",
-    root_edge="/home/qi.xiong/StereoMatching/IGEV-Improve/data/sceneflow/ggtedge",
+    root_disp="./data/sceneflow/disparity",
+    root_edge="./data/sceneflow/ggtedge",
     split_list=("TRAIN", "TEST"),
     grad_thresh=2.5,
     mode="sobel",
