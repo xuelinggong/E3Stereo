121216ec8cba6fb695f2d86c7c7175769c0a6164
xuelinggong <18365553626@163.com>
2026-03-05 13:30:32 +0800

    Update eval_edge.sh

--- eval_edge.sh
+++ eval_edge.sh
@@ -2,9 +2,9 @@
 # 几何边缘模型评估与可视化
 # 用法：bash eval_edge.sh
 
-CKPT=/home/qi.xiong/StereoMatching/IGEV-Improve/EStereo/checkpoints/geo-edge-sceneflow-1.5/geo-edge-sceneflow-1.5_best.pth
-DATA_ROOT=/home/qi.xiong/StereoMatching/IGEV-Improve/data/sceneflow
-OUTPUT_DIR=/home/qi.xiong/StereoMatching/IGEV-Improve/EStereo/eval_edge_vis
+CKPT=./StereoMatching/IGEV-Improve/EStereo/checkpoints/geo-edge-sceneflow-1.5/geo-edge-sceneflow-1.5_best.pth
+DATA_ROOT=./StereoMatching/IGEV-Improve/data/sceneflow
+OUTPUT_DIR=./StereoMatching/IGEV-Improve/EStereo/eval_edge_vis
 NUM_VIS=100
 SPLIT=TEST
 
